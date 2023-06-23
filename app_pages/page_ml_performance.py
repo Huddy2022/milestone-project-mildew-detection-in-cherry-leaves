e6ce8b36adf2ed2580140eb66a2bec758cefcd2e
Huddy2022 <>
2023-06-23 20:19:08 +0000

    add plot images and write up for page_ml_performance

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -9,11 +9,19 @@ def page_ml_performance_metrics():
     version = 'v2'
 
     st.write("### Train, Validation and Test Set: Labels Frequencies")
-
-    labels_distribution = plt.imread(
-        f"outputs/{version}/labels_distribution.png")
-    st.image(labels_distribution,
-             caption='Labels Distribution on Train, Validation and Test Sets')
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        labels_distribution = plt.imread(
+            f"outputs/{version}/labels_distribution.png")
+        st.image(labels_distribution,
+                 caption='Labels Distribution on Train, '
+                         'Validation and Test Sets')
+    with col2:
+        sets_distribution = plt.imread(
+            f"outputs/{version}/sets_distribution_pie.png")
+        st.image(sets_distribution,
+                 caption='Datasets Distribution on Train, '
+                         'Validation and Test Sets')
     st.write("---")
 
     st.write("### Model History")
@@ -29,3 +37,19 @@ def page_ml_performance_metrics():
     st.write("### Generalised Performance on Test Set")
     st.dataframe(pd.DataFrame(load_test_evaluation(
         version), index=['Loss', 'Accuracy']))
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        confusion_matrix = plt.imread(
+            f"outputs/{version}/confusion_matrix.png")
+        st.image(confusion_matrix, caption="Confusion Matrix")
+    with col2:
+        clf_report = plt.imread(f"outputs/{version}/clf_report.png")
+        st.image(clf_report, caption="classification report")
+
+    st.write(
+        f"* The overall accuracy of 99.88% and a loss of 0.0041 on "
+        f"identifying infected cherry leaves indicate the successful "
+        f"performance of this machine learning model in determining the "
+        f"presence of powdery mildew. These results align with the defined "
+        f"success metrics in the ML business requirements, with a minimum "
+        f"degree of accuracy of 97% on identifying infected cherry leaves.")
